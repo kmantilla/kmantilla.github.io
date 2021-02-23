d3f59eb8f571388c490f316ec2124627ee00d0a2
Khiel Mantilla <kmantilla@Khiels-MacBook-Pro.local>
2021-02-23 13:04:41 -0500

    design changes, removed animations, added blog link

--- js/animation.js
+++ js/animation.js
@@ -1,2 +1,2 @@
-gsap.from('.jumbo', {opacity: 0, duration: 1, delay: 2});
-gsap.from('#jumbo-text', {color: "#000", duration: 1, delay: 2});
+// gsap.from('.jumbo', {opacity: 0, duration: 1, delay: 2});
+// gsap.from('#jumbo-text', {color: "#000", duration: 1, delay: 2});
